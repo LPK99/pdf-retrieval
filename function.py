a6cbc5ee15ac84cac9940b28c0de8757d4855fdf
Your Name <you@example.com>
2023-09-06 23:24:05 +0700

    updated requirements.txt

--- function.py
+++ function.py
@@ -1,5 +1,4 @@
 from PyPDF2 import PdfReader
-from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 import streamlit as st
